076a31c5a811a0ba6a59f2ef9619f484f1813556
garethhough <garethhough@googlemail.com>
2022-06-23 18:19:47 +0100

    Edit function is now working.  --edit --title --actor --releaseDate

--- src/app.js
+++ src/app.js
@@ -1,29 +1,51 @@
-require("./db/connection")
+require("./db/connection");
 
-const mongoose = require ("mongoose");
-const yargs = require ("yargs");
-const { addMovie, listMovies, deleteMovie } = require ("./movies/movieMethods");
-
+const mongoose = require("mongoose");
+const yargs = require("yargs");
+const {
+  addMovie,
+  listMovies,
+  updateMovie,
+  removeMovie,
+} = require("./movies/movieMethods");
 
 const app = async (yargsObj) => {
-    try {
-        // if add is typed into terminal, addMovie function is called
-        if (yargsObj.add) {
-            await addMovie({title: yargsObj.title, actor: yargsObj.actor, releaseDate: yargsObj.releaseDate,})
-        // after add function is called, listMovies function is called to show you the contents   
-            console.log(await listMovies());
-        // if list is typed into terminal, listMovies function is called    
-        } else if (yargsObj.list) {
-            console.log(await listMovies());
-        } else {
-            console.log("Incorrect command");
-        }
-
-        await mongoose.disconnect();
-    } catch (error) {
-        console.log(error);
+  try {
+    // if add is typed into terminal, addMovie function is called
+    if (yargsObj.add) {
+      await addMovie({
+        title: yargsObj.title,
+        actor: yargsObj.actor,
+        releaseDate: yargsObj.releaseDate,
+      });
+      // after add function is called, listMovies function is called to show you the contents
+      console.log(await listMovies());
+      // if list is typed into terminal, listMovies function is called
+    } else if (yargsObj.list) {
+      console.log(await listMovies());
+      // if remove is typed into terminal, removeMovie function is called
+    } else if (yargsObj.remove) {
+      await removeMovie({
+        title: yargsObj.title,
+        actor: yargsObj.actor,
+        releaseDate: yargsObj.releaseDate,
+      });
+      console.log(await listMovies());
+      // if update is typed into terminal, updateMovie function is called
+    } else if (yargsObj.update) {
+      await updateMovie({
+        title: yargs.argv.title,
+        actor: yargs.argv.actor,
+        releaseDate: yargs.argv.releaseDate,
+      });
+      console.log(updateMovie);
+    } else {
+      console.log("Incorrect command");
     }
-}
+    await mongoose.disconnect();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app(yargs.argv);
- 
--- src/movies/movieMethods.js
+++ src/movies/movieMethods.js
@@ -1,26 +1,33 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const Movie = require("./movieModels");
-
 
 // movieObj is the data passed in from terminal
 
 exports.addMovie = async (movieObj) => {
-    try {
-        await Movie.create(movieObj)
-
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await Movie.create(movieObj);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.listMovies = async () => {
-    try {
-        return await Movie.find({});
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return await Movie.find({});
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-//CREATE AN UPDATE ONE OR UPDATE MANY FUNCTION
+//find movie by title name and update title, actor, and release date
+    exports.updateMovie = async (movieObj) => {
+      try {
+        await Movie.updateOne({title: movieObj.title, actor: movieObj.actor, releaseDate: movieObj.releaseDate});
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    //to do list
 //CREATE DELETE ONE OF DELETE MANY FUNCTION
+
